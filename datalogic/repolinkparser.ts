02f87e18f7d096eca211d968b685d9fa99291540
zixian92 <zixian1992@hotmail.com>
2016-10-18 16:24:58 +0800

    Fixes repo link parser implementation

--- datalogic/repolinkparser.ts
+++ datalogic/repolinkparser.ts
@@ -6,16 +6,27 @@
  */
 
 export default class RepoLinkParser {
+  private static prefix: string = 'https://github.com/';
+  private static format: string = 'https://github.com/<owner>/<repo>';
 
   /**
    * Extracts repo owner and repo name from given repo URL.
-   * @param {string} url
-   * @return {{owner: string, repo: string}}
+   * @param {string} url of the format https:/github.com/<owner>/<repo>
+   * @return {{owner: string, repo: string}} or null if error occurred.
    */
   getRepoFromURL(url:string){
+    if(!url.startsWith(RepoLinkParser.prefix)){
+      console.log(`URL should be of the format: ${RepoLinkParser.format}`);
+      return null;
+    }
+    var parts = url.replace(RepoLinkParser.prefix, '').split('/');
+    if(parts.length<2){
+      console.log(`URL should be of the format: ${RepoLinkParser.format}`);
+      return null;
+    }
     return {
-      owner: 'john',
-      repo: 'some_repo'
+      owner: parts[0],
+      repo: parts[1]
     };
   }
 }
--- tests/datalogic/repolinkparsertest.ts
+++ tests/datalogic/repolinkparsertest.ts
@@ -5,7 +5,13 @@ describe('RepoLinkParserTest', () => {
   describe('#parsetest', () => {
     var parser: RepoLinkParser = new RepoLinkParser();
     it('should only have owner name and repo name', () => {
-      assert.deepEqual(parser.getRepoFromURL('https://github.com/ZiXian92/MyGitHubIssueTracker'), {owner: 'ZiXian92', repo: 'MyGithubIssueTracker'});
+      assert.deepEqual(parser.getRepoFromURL('https://github.com/ZiXian92/MyGitHubIssueTracker'), {owner: 'ZiXian92', repo: 'MyGitHubIssueTracker'});
+    });
+
+    it('should be null for invalid Github repo URL', () => {
+      assert.isNull(parser.getRepoFromURL('http://github.com/zixian92/mygithubissuetracker'));
+      assert.isNull(parser.getRepoFromURL('https://github.com/zixian92'));
+      assert.isNull(parser.getRepoFromURL('http://github.com/zixian92/mygithubissuetracker'));
     })
   });
 });
