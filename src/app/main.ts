03ce7630d0952e873d6da71c6f15fb38884dd7ee
Kyle Leong Wei Ming <kylelwm@gmail.com>
2016-10-22 14:32:08 +0800

    Added bootstrap and chartjs as import into angular 2

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,4 +2,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
+import "../styles/bootstrap.min.css";
+import "../styles/bootstrap-flex.min.css";
+import "../styles/bootstrap-grid.min.css";
+import "../styles/bootstrap-reboot.min.css";
+
+// To import external JS libraries
+// Heres to 3 hours of my time...
+import "../../node_modules/chart.js/src/chart.js";
+
 platformBrowserDynamic().bootstrapModule(AppModule);
