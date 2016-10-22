ef064f3990ee583fdc367a211779e6422a6b82b9
Kyle Leong Wei Ming <kylelwm@gmail.com>
2016-10-22 14:29:10 +0800

    Added bootstrap and chartjs as dependencies

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,3 +4,5 @@
 /// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+
+/// <reference path="node_modules/chart.js/src/chart.js" />
