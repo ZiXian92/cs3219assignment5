7f5e17c29823f64b5c5b910a235e79ec62041010
zixian92 <zixian1992@hotmail.com>
2016-10-25 13:14:25 +0800

    Adds endpoint to list contributors

---
+++ datalogic/processors/contributor.name.ts
@@ -0,0 +1,12 @@
+/**
+ * datalogic/contributor.name.ts
+ * Filter component to extract name of contributor from contributor list.
+ * @author zixian92
+ */
+
+'use strict';
+
+export function extractContributorName(contributors: any[]): string[] {
+  return contributors.map((c: any): string => (c.author && c.author.login)? c.author.login: null)
+  .filter((c: string): boolean => !!c);
+}
