cc54cfc9cc445ee5213a3d0f55ae60bdf0a69731
zixian92 <zixian1992@hotmail.com>
2016-10-30 14:33:52 +0800

    Adds redis caching for contributor summary

--- server/api/contributors.ts
+++ server/api/contributors.ts
@@ -24,17 +24,15 @@ ContributorRouter.get('/:owner/:repo', (req: Request, res: Response, next: NextF
   let repo: Repository = { owner: req.params.owner, name: req.params.repo };
   let redisKey: string = `/api/contributors/${repo.owner}/${repo.name}`;
   let ttl: number = 3600;
-  connectToRedisAndDo((conn: RedisClient) => new Promise((resolve, reject) =>
-    conn.get(redisKey, (err: any, contributors: string[]) => {
+  connectToRedisAndDo((conn: RedisClient): Promise<any> => new Promise((resolve, reject) =>
+    conn.get(redisKey, (err: any, contributors: string) => {
       if(err) reject(err);
       else resolve(contributors);
     })
   )).then((contributors: string): any => {
     if(contributors){
-      console.log('Using redis cache data');
       return res.json(JSON.parse(contributors));
     }
-    console.log('Fetching from Github API');
     return new PromisePipe(getContributors, extractContributorName).processData({ repo })
     .then((contributors: string[]): Promise<any> =>
       connectToRedisAndDo((conn: RedisClient) => new Promise((resolve, reject): any =>
@@ -48,10 +46,25 @@ ContributorRouter.get('/:owner/:repo', (req: Request, res: Response, next: NextF
 
 ContributorRouter.get('/:owner/:repo/summary', (req: Request, res: Response, next: NextFunction): void => {
   let repo: Repository = { owner: req.params.owner, name: req.params.repo };
-  let pipe: PromisePipe = new PromisePipe(
-    getContributors, computeContributorTotal
-  );
-  pipe.processData({
-    repo
-  }).then((responseData: ContributorTotalStats[]): any => res.json(responseData));
+  let redisKey: string = `/api/contributors/${repo.owner}/${repo.name}/summary`;
+  let ttl: number = 3600;
+  connectToRedisAndDo((conn: RedisClient): Promise<any> => new Promise((resolve, reject) =>
+    conn.get(redisKey, (err: any, summary: string) => {
+      if(err) reject(err);
+      else resolve(summary);
+    })
+  )).then((summary: string): any => {
+    if(summary){
+      return res.json(JSON.parse(summary));
+    } else {
+      return new PromisePipe(getContributors, computeContributorTotal).processData({ repo })
+      .then((responseData: ContributorTotalStats[]): any =>
+        connectToRedisAndDo((conn: RedisClient): Promise<any> => new Promise((resolve, reject): any =>
+          conn.setex(redisKey, ttl, JSON.stringify(responseData), (err: any): any => err? reject(err): resolve()))
+        .then((): any => res.json(responseData))
+      ));
+    }
+  }).catch((err: any): void => {
+    console.log(err); res.status(500);
+  });
 });
