d5a4e92e3543fedb16d414d4d9fc4fd2ef045281
zixian92 <zixian1992@hotmail.com>
2016-11-03 23:20:59 +0800

    Makes file tree endpoint use authenticated request if possible

--- server/api/files.ts
+++ server/api/files.ts
@@ -20,27 +20,37 @@ export const FilesRouter = Router();
 FilesRouter.get('/:owner/:repo', (req: Request, res: Response, next: NextFunction): void => {
   let reqData: RepoRequest = {
     repo: { owner: req.params.owner, name: req.params.repo },
-    data: {
-      branch: req.query.branch || 'master'
-    }
+    data: { branch: req.query.branch || 'master' }
   };
+  let user: string = req['user']? req['user'].user: undefined;
   let redisKey: string = `/api/trees/${reqData.repo.owner}/${reqData.repo.name}?breanch=${reqData['branch']}`;
   let ttl: number = 3600;
+
   connectToRedisAndDo((conn: RedisClient): Promise<any> => new Promise((resolve, reject) =>
-    conn.get(redisKey, (err: any, tree: string): any => err? reject(err): resolve(tree))
-  )).then((treeStr: string): any => {
-    if(treeStr){
-      return res.json(JSON.parse(treeStr));
-    } else{
-      return new PromisePipe(getFiles).processData(reqData)
-      //return new PromisePipe(getFiles, extractFilePaths).processData(reqData)
-      .then((filePaths: string[]): Promise<any> =>
-        connectToRedisAndDo((conn: RedisClient): Promise<any> => new Promise((resolve, reject) =>
-          conn.setex(redisKey, ttl, JSON.stringify(filePaths), (err: any): any => err? reject(err): resolve())
-        )).then((): any => res.json(filePaths))
-      );
-    }
+    conn.get(redisKey, (err: any, tree: string): any => {
+      if(err){ console.log(`Failed to get cached file tree for ${reqData.repo.owner}/${reqData.repo.name}`); console.log(err); }
+      if(tree) return resolve({ treeStr: tree });
+      if(user) conn.get(user, (err: any, token: string): any => {
+        if(err){ console.log(`Failed to get access token for ${user}`); console.log(err); }
+        return resolve({ token });
+      });
+      else return resolve({});
+    })
+  )).then(({ treeStr = null, token = null }): any => {
+    if(treeStr) return res.json(JSON.parse(treeStr));
+    if(token) reqData.requestor = token;
+    return new PromisePipe(getFiles).processData(reqData)
+    //return new PromisePipe(getFiles, extractFilePaths).processData(reqData)
+    .then((filePaths: string[]): void => {
+      connectToRedisAndDo((conn: RedisClient): Promise<any> => new Promise((resolve, reject) =>
+        conn.setex(redisKey, ttl, JSON.stringify(filePaths), (err: any): any => {
+          if(err){ console.log(`Failed to cache file tree for ${reqData.repo.owner}/${reqData.repo.name}`); console.log(err); }
+          resolve();
+        })
+      ));
+      res.json(filePaths);
+    });
   }).catch((err: any): void => {
-    console.log(err); res.status(500);
+    console.log(err); res.sendStatus(500);
   });
 });
